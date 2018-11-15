4850840530240eb9cd07b6d7183d7b9812165564
John Meguerian <johnmeguerian@gmail.com>
2018-11-15 07:47:12 -0500

    basic methods working

--- pages/index.jsx
+++ pages/index.jsx
@@ -1,5 +1,34 @@
-const Homepage = () => {
-  return <div>hello world.</div>
+import React from 'react'
+
+class App extends React.Component {
+  constructor(props) {
+    super(props)
+    this.state = { message: '', reversedMessage: '' }
+  }
+
+  formatMessage = () => {
+    let result = this.state.message
+      .split(' ')
+      // for each word split the word into chars
+      .map(w => w.split('').join('\xa0'))
+      // join each word array with non-breaking space
+      // .join('\xa0\xa0')
+      .join(' ')
+      .toUpperCase()
+
+    this.setState({ reversedMessage: result })
+  }
+  render() {
+    return (
+      <React.Fragment>
+        <div>the message is: {this.state.message}</div>
+        <div>
+          the result is: <span>{this.state.reversedMessage}</span>
+        </div>
+        <button onClick={this.formatMessage}>Format</button>
+      </React.Fragment>
+    )
+  }
 }
 
-export default Homepage
+export default App
