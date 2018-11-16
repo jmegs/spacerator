72404c98ea5384b2d5263fc177913b6aa16fead6
John Meguerian <johnmeguerian@gmail.com>
2018-11-15 19:54:13 -0500

    css working

--- pages/index.jsx
+++ pages/index.jsx
@@ -1,6 +1,8 @@
 import React from 'react'
 import Clipboard from 'react-clipboard.js'
 
+import '../style.css'
+
 class Spacerator extends React.Component {
   constructor(props) {
     super(props)
