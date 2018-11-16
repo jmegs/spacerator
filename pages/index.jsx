75e5c6a63cbd91d6d76d7d260b3299d27900adf7
John Meguerian <johnmeguerian@gmail.com>
2018-11-15 21:33:20 -0500

    clear button working

--- pages/index.jsx
+++ pages/index.jsx
@@ -6,30 +6,41 @@ import '../style.css'
 class Spacerator extends React.Component {
   constructor(props) {
     super(props)
-    this.state = { input: '', output: null }
+    this.state = { input: '', output: null, dirty: false }
   }
 
   handleChange = event => {
     let userInput = event.target.value
-    this.setState({ input: userInput, output: this.formatMessage(userInput) })
+    this.setState({
+      input: userInput,
+      output: this.letterspace(userInput),
+      dirty: !!this.state.input.length
+    })
   }
 
-  formatMessage = string => {
+  clearInput = () => {
+    this.setState({ input: '', output: '', dirty: false })
+  }
+
+  letterspace = string => {
     return (
       string
+        // split the string into words
         .split(' ')
+
         // for each word split the word into chars
+        // then join them on a non-breaking space
         .map(w => w.split('').join('\xa0'))
-        // join each word array with non-breaking space
-        // .join('\xa0\xa0')
+
+        // take those spaced words and join them into a sentence
+        // using a non-breaking plus and breaking space
         .join('\xa0 ')
+
+        // then transform them to upper-case characters
         .toUpperCase()
     )
   }
 
-  getText = () => {
-    return this.state.output
-  }
   render() {
     return (
       <main>
@@ -38,13 +49,21 @@ class Spacerator extends React.Component {
           Type into the input and then copy the letterspaced result. This is
           really useful for tracking out capital letters in Google Slides.
         </p>
-        <input
-          type="text"
-          placeholder="type something…"
-          onChange={this.handleChange}
-        />
+        <span className="inputContainer">
+          <input
+            value={this.state.input}
+            type="text"
+            placeholder="type something…"
+            onChange={this.handleChange}
+          />
+          {this.state.dirty && (
+            <div className="inputContainer__clear" onClick={this.clearInput}>
+              <span>&times;</span>
+            </div>
+          )}
+        </span>
         <div className="result">
-          <p className="result__text">{this.formatMessage(this.state.input)}</p>
+          <p className="result__text">{this.state.output}</p>
           <Clipboard
             data-clipboard-text={this.state.output}
             className="result__button"
